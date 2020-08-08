8ef329d8ac1a5053f774055a72043e910f88dad1
Stella Vilar <vilar.stella@gmail.com>
2020-08-08 21:36:03 +0200

    React chat elements installed for Chatbox formatting

--- chat-application/src/components/ChatBox.js
+++ chat-application/src/components/ChatBox.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Segment, Header, Input } from 'semantic-ui-react';
+import { MessageBox } from 'react-chat-elements';
+
+import 'react-chat-elements/dist/main.css';
 
 import { sendMessage, syncMessage } from '../store/actions';
+
 
 const ChatBox = () => {
     const dispatch = useDispatch();
     const currentMessage = useSelector((state) => state.message);
     const messages = useSelector((state) => state.messages)
-     .map((message) => <li key={message.id}>{message.content}</li>)
+     .map((message, index) => <MessageBox 
+                        key={index}
+                        position={'right'}
+                        type={'text'}
+                        text={message.content}
+                        title={message.author}
+                        />)
   
     return (
         <div className="chat-box">
             <Header as="h3">Contact name</Header>
             <Segment>
                 <Segment className="text-area">
-                    <ul>{messages}</ul>
+                    {messages}
                 </Segment>
                 <form
                  onSubmit={(e) => {
